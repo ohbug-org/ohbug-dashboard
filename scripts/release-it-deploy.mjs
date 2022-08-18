9dc55e2d3d1afdcb1268e86b3ec3e2d96218bedf
chenyueban <jasonchan0527@gmail.com>
2022-08-18 16:30:10 +0800

    chore: update release-it

--- scripts/release-it-deploy.mjs
+++ scripts/release-it-deploy.mjs
@@ -8,8 +8,7 @@ class Deploy extends Plugin {
     const name = `${imageName}:${tagName}`
     const raw = `#!/bin/sh
 
-docker buildx create --use --name multi-arch-builder
-docker buildx build --platform linux/amd64,linux/arm64 -t ${name} .`
+docker buildx build --platform linux/amd64,linux/arm64 -t ${name} -t ${imageName}:latest .`
     writeFileSync('./scripts/buildDockerImage.sh', raw, {
       encoding: 'utf8',
       mode: 0o777,
