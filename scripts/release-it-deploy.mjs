9f7c378c5287d6ddf0c52d8dee05c9b6ab1f86e7
chenyueban <jasonchan0527@gmail.com>
2022-08-18 16:06:40 +0800

    chore: change release-it deploy order

--- scripts/release-it-deploy.mjs
+++ scripts/release-it-deploy.mjs
@@ -1,11 +1,18 @@
+import { writeFileSync } from 'fs'
 import { Plugin } from 'release-it'
-import { execa } from 'execa'
 
 class Deploy extends Plugin {
   afterRelease() {
     const tagName = this.config.contextOptions.tagName || this.config.contextOptions.version || 'latest'
-    const imageName = `ohbug/ohbug-ce:${tagName}`
-    await execa('docker', ['buildx', 'build', '--platform', 'linux/amd64,linux/arm64', '-t', imageName], { stdout: 'inherit' })
+    const imageName = 'ohbug/ohbug-ce'
+    const name = `${imageName}:${tagName}`
+    const raw = `#!/bin/bash
+
+docker buildx build --platform linux/amd64,linux/arm64 -t ${imageName}
+
+echo Successfully released ${name}
+`
+    writeFileSync('./buildDockerImage.sh', raw, 'utf8')
   }
 }
 
