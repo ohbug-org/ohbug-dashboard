28c32f15bd61662264998e58ef00e2c3825a796f
chenyueban <jasonchan0527@gmail.com>
2022-08-18 16:25:00 +0800

    chore: update release-it

--- scripts/release-it-deploy.mjs
+++ scripts/release-it-deploy.mjs
@@ -3,15 +3,13 @@ import { Plugin } from 'release-it'
 
 class Deploy extends Plugin {
   beforeRelease() {
-    const tagName = this.config.contextOptions.tagName || this.config.contextOptions.version || 'latest'
     const imageName = 'ohbug/ohbug-ce'
+    const tagName = this.config.contextOptions.tagName || this.config.contextOptions.version || 'latest'
     const name = `${imageName}:${tagName}`
-    const raw = `#!/bin/bash
+    const raw = `#!/bin/sh
 
-docker buildx build --platform linux/amd64,linux/arm64 -t ${imageName}
-
-echo Successfully released ${name}
-`
+docker buildx create --use --name multi-arch-builder
+docker buildx build --platform linux/amd64,linux/arm64 -t ${name}`
     writeFileSync('./scripts/buildDockerImage.sh', raw, {
       encoding: 'utf8',
       mode: 0o777,
