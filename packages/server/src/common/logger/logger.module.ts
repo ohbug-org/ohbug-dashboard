48fadbe6f7b3db62eb07e955df2d454820bdb47d
chenyueban <jasonchan0527@gmail.com>
2022-08-30 16:36:45 +0800

    fix(server): fixed logger problem

--- packages/server/src/common/logger/logger.module.ts
+++ packages/server/src/common/logger/logger.module.ts
@@ -5,18 +5,7 @@ import pino from 'pino'
 @Module({
   imports: [
     LoggerModuleBase.forRoot({
-      pinoHttp: {
-        name: 'log',
-        level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
-        transport: process.env.NODE_ENV !== 'production'
-          ? { target: 'pino-pretty', options: { singleLine: true } }
-          : undefined,
-        stream: pino.destination({
-          dest: './logs.log',
-          minLength: 4096,
-          sync: false,
-        }),
-      },
+      pinoHttp: { stream: pino.destination('./logs.log') },
       useExisting: true,
     }),
   ],
